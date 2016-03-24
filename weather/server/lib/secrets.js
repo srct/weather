6c5a31ffa868190b2d75adf20d52faffb2e06370
Zach <zmknox@gmail.com>
2016-03-23 22:20:35 -0400

    connected to forcast.io 😄

---
+++ weather/server/lib/secrets.js
@@ -0,0 +1,3 @@
+Meteor.startup(function () {
+  process.env.API_KEY = 'bed7e55eda48440c2da713bdc5e93207';
+});
--- weather/weather.js
+++ weather/weather.js
@@ -1,4 +1,8 @@
 if (Meteor.isClient) {
+    Meteor.call('weatherDataForLoc', '38.830295', '-77.307717', function(err, res){
+        console.log(res);
+        //Sends the result of the API call to the browser console, will change
+    });
 
   Template.weather.helpers({
 
@@ -13,4 +17,15 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+
+Meteor.methods({
+    'weatherDataForLoc': function (lat, long) {
+        //API_KEY is an environmental veriable, you can set it with a JS file in
+        // server/lib or in your server control panel.
+        var API_KEY = process.env.API_KEY;
+        var apiURL = 'https://api.forecast.io/forecast/' + API_KEY + '/' + lat + ',' + long;
+        var response = HTTP.get(apiURL).data;
+        return response;
+    }
+});
 }
