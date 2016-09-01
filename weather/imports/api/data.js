19e187420f3755d6bf492e2df41ad16d51ac2647
Zach Knox <zmknox@gmail.com>
2016-08-31 21:00:54 -0400

    Wrap the data insert in a function to call it later
    probably gonna need to do more with this

--- weather/imports/api/data.js
+++ weather/imports/api/data.js
@@ -3,17 +3,19 @@ import { Mongo } from 'meteor/mongo';
 
 export const WeatherData = new Mongo.Collection('weather-data');
 
-for(var key in LOCATIONS) {
-    const current = LOCATIONS[key];
-    var currentData
-    Meteor.call('weatherDataForLoc', current.lat, current.long, function(err, res){
-        currentData = res
-    });
-    WeatherData.insert ({
-        name: current.name,
-        lat: current.lat,
-        long: current.long,
-        data: currentData,
-        lastUpdated: new Date(),
-    });
+export function refreshData() {
+    for(var key in LOCATIONS) {
+        const current = LOCATIONS[key];
+        var currentData
+        Meteor.call('weatherDataForLoc', current.lat, current.long, function(err, res){
+            currentData = res
+        });
+        WeatherData.insert ({
+            name: current.name,
+            lat: current.lat,
+            long: current.long,
+            data: currentData,
+            lastUpdated: new Date(),
+        });
+    }
 }
