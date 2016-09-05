dde2f49d04bb1bd876de2d2d2f06c3a8d5b3e6f0
David Haynes <dhaynes207@gmail.com>
2016-09-04 23:25:07 -0400

    Massive cleanup & renaming & moving & removing
    - stick close to the meteor file structure
    - properly use client/server directories
    - renaming files to better represent what they are
    - more appropriate locations for things
    - remove some dummy code (will be back soon)

--- weather/imports/api/weather-data.js
+++ weather/imports/api/weather-data.js
@@ -1,7 +1,18 @@
-"use strict";
 import { Mongo } from 'meteor/mongo';
 
 export const WeatherData = new Mongo.Collection('weather-data');
+
+Meteor.methods({
+    'weatherDataForLoc': function (lat, long) {
+        // API_KEY is an environmental veriable, you can set it with a JS file in
+        // server/lib or in your server control panel.
+        var API_KEY = process.env.API_KEY;
+        var apiURL = 'https://api.forecast.io/forecast/' + API_KEY + '/' + lat + ',' + long;
+        var response = HTTP.get(apiURL).data;
+        return response;
+    }
+});
+
 
 export function refreshData() {
     for(var key in LOCATIONS) {
--- weather/server/main.js
+++ weather/server/main.js
@@ -1,1 +1,8 @@
-import { WeatherData } from '../imports/api/data.js';
+// server entry point, imports all server code
+import { Meteor } from 'meteor/meteor';
+
+import { WeatherData } from '../imports/api/weather-data.js';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
