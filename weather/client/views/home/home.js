d12ab2c5bbec0554fb685324b25ba94132c2b501
Manuel Gauto <mgauto@mgenterprises.org>
2016-11-24 12:27:24 -0500

    Now with fewer F-words.

--- weather/client/views/home/home.js
+++ weather/client/views/home/home.js
@@ -1,14 +1,11 @@
 Template.home.events({
   'click #fairfax-tab-button' (event) {
-    alert("FUCK");
   },
   'click a.mdl-layout__tab-bar' (event) {
-    console.log("FUCK");
     console.log(event);
   }
 });
 Template.home.onRendered(function() {
   document.getElementById("#fairfax-tab-button").addEventListener("click", function(){
-    alert("FUCK");
   });
 });
