f6ac66557432338bedb02c5b7952922b87072653
David Haynes <dhaynes207@gmail.com>
2017-01-24 21:28:39 -0500

    Remove SimpleSchema since it is not being used
    - may come back in the future

--- weather/imports/api/weather-data.js
+++ weather/imports/api/weather-data.js
@@ -1,26 +1,3 @@
 import { Mongo } from 'meteor/mongo';
 
 export const WeatherData = new Mongo.Collection('WeatherDatas');
-
-WeatherData.schema = new SimpleSchema({
-    name: {
-        type: String,
-        label: "The name of the GMU Campus"
-    },
-    lat: {
-        type: Number,
-        label: "The latitude of the GMU Campus"
-    },
-    long: {
-        type: Number,
-        label: "The longiute of the GMU Campus"
-    },
-    data: {
-        type: Object,
-        label: "The most current weather entry for this location"
-    },
-    lastUpdated: {
-        type: Date,
-        label: "The last time that this campus's current forecast was updated"
-    }
-})
